63c63bb311d6fc00c116d8fb4ecde3ccece987e9
Samuel Sanchez <samuelestebansanchez08@gmail.com>
2022-03-05 15:04:51 -0500

    Terminacion de agendar vuelos

---
+++ main3.js
@@ -0,0 +1,28 @@
+
+var passw = document.getElementById('passw'),
+    privado = document.getElementById('privado'),
+    candado = 0;
+
+function cambio() {
+    if (candado == 0) {
+        privado.style.display = 'block';
+        candado = 1;
+    }
+    else {
+        privado.style.display = 'none';
+        candado = 0
+    }
+}
+
+// passw.addEventListener('click',cambio)
+passw.addEventListener('click', cambio)
+
+
+
+let button = document.getElementById("Agenda");
+
+if (button) {
+    button.addEventListener('click', () => {
+        alert("Tu vuelo se agendo! :)")
+    })
+}
